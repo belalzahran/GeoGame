5a5644f2563b731af74be478887018708f1d2a94
Belal Zahran <belalzahran@gmail.com>
2024-08-30 18:18:57 -0700

    Added auto correct with fuzzy wuzzy

--- countries_game/country_game.py
+++ countries_game/country_game.py
@@ -1,5 +1,7 @@
 from typing import List, Dict
 from geonames_api import RestAdapter
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 class CountryGame:
 
@@ -26,10 +28,10 @@ class CountryGame:
 
         unfiltered_api_countries_list = self.get_countries_from_API()
         if continent is None:
-            result = [country['countryName'] for country in unfiltered_api_countries_list]
+            result = [country['countryName'].lower() for country in unfiltered_api_countries_list]
            
         else:
-            result = [country["countryName"] for country in unfiltered_api_countries_list if country['continentName'] == continent]
+            result = [country["countryName"].lower() for country in unfiltered_api_countries_list if country['continentName'] == continent]
         
         return result
 
@@ -47,7 +49,7 @@ class CountryGame:
 
     def get_score(self) -> str:
 
-        return "{} correct countries answered out of {}".format(len(self.correct_user_guesses),len(self.correct_answers))
+        return "{} out of {}".format(len(self.correct_user_guesses),len(self.correct_answers))
 
     def start_game_introduction(self):
 
@@ -55,7 +57,7 @@ class CountryGame:
 
         print("The objective of the game is to guess as many countries as you can correctly!")
 
-        print("Also, capatilizatoin does not matter and make sure to leave spaces between your guesses!\n")
+        print("Also, capatalizatoin does not matter and make sure to separate your answers with commmas and spaces!\n")
         print(f"Game Mode: {self.game_mode}")
         if self.continent is not None:
             print(f"Continent: {self.continent}")
@@ -68,21 +70,39 @@ class CountryGame:
         self.start_game_introduction()
 
 
-        user_input = input()
+        user_input = input("\t")
 
         while self.game_won is False:
 
             self.parse_user_input(user_input)
 
+            print(f"\n\t\t{self.get_score()}\n")
+
+            user_input = input("\t")
+
+
+        print(f"Congrats, you beat the game with a score of {self.get_score()}")
         
 
     def parse_user_input(self, user_input: str):
 
-        guesses = user_input.split(" ")
+        guesses = user_input.split(", ")
+
 
         for guess in guesses:
-            if guess in self.correct_answers:
-                self.correct_user_guesses.append(guess)
+            raw_guess = guess
+            guess = guess.lower()
+        
+
+            closest_match, score = process.extractOne(guess, self.correct_answers, scorer=fuzz.ratio)
+
+            if score >= 80:
+
+                if score <= 99:
+                    print(f"\n\t\tAuto Correcting {raw_guess} to {closest_match}. Similarity Score: {score}/100\n")
+                self.correct_user_guesses.append(closest_match)
             else:
                 self.incorrect_user_guesses.append(guess)
                 
+        if len(self.correct_user_guesses) == len(self.correct_answers):
+            self.game_won = True
